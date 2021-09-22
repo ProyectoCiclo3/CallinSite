be2b5b2a4e6d18f8fe32c565425e036122c5ad5b
ProyectoCiclo3 <proyectoclase72@gmail.com>
2021-09-21 19:58:35 -0500

    Estructura_Proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- Nav bar -->
+    <nav
+      class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row"
+    >
+      <div class="container">
+        <a class="navbar-brand float-left">CALLING SITE</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/">Registrar Empleado...</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/view">Ver empleados...</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Router view -->
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ListComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!--Listar Contenido-->
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return{};
+    },
+};
+</script>
